20e0bd2fda7770fbb560ea7bee1625adab7aa33f
Evgeniy Nazarov <zentorno.2000@mail.ru>
2025-03-23 21:18:19 +0300

    10lr

---
+++ Backend-lection-EF/Controllers/FilesController.cs
@@ -0,0 +1,102 @@
+﻿using System.Net;
+using System.Text;
+using Backend_lection_EF.Models;
+using Backend_lection_EF.Models.Data;
+using Backend_lection_EF.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_lection_EF.Controllers;
+
+[ApiController]
+[Route("api/[action]")]
+public class FilesController : ControllerBase
+{
+    private readonly AppDbContext _dbContext;
+
+    // с помощью Dependency Injection получаем экземпляр контекста базы данных
+    public FilesController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetJson(int id)
+    {
+        try
+        {
+            // получаем данные из базы данных
+            var entity = _dbContext.Entities.Find(id);
+            // если данных не найдено, возвращаем статус-код 404
+            if (entity is null)
+            {
+                return NotFound();
+            }
+
+            // возвращаем данные json со статус-кодом 200
+            return Ok(entity);
+        }
+        catch (Exception e)
+        {
+            // в случае ошибки возвращаем статус-код 400 с сообщением "Ошибка"
+            Console.WriteLine(e);
+            return BadRequest("Ошибка");
+        }
+    }
+
+    [HttpGet]
+    public IResult GetHtml()
+    {
+        // с помощью созданного класса HtmlResult возвращаем html
+        const string htmlContent = "<h1>Имя сотрудника:</h1><span>Иван</span>";
+        return Results.Extensions.Html(@$"<!doctype html> <html><body>{htmlContent}</body></html>");
+    }
+
+    [HttpGet]
+    public async Task<IResult> GetFile()
+    {
+        try
+        {
+            // путь к существующему файлу
+            const string path = "Public/vk.jpg";
+            // считываем файл в массив байтов
+            var fileContent = await System.IO.File.ReadAllBytesAsync(path);
+            // устанавливаем тип контента
+            const string contentType = "image/jpeg";
+            // задаём название скачиваемому файлу (может отличаться от "базового" названия)
+            const string downloadName = "vk.jpg";
+            // возвращаем файл
+            return Results.File(fileContent, contentType, downloadName);
+        }
+        catch (Exception e)
+        {
+            // в случае ошибки возвращаем статус-код 400 с сообщением "Ошибка"
+            Console.WriteLine(e);
+            return Results.BadRequest(e);
+        }
+    }
+
+    [HttpGet]
+    public IResult GetFileWithStream()
+    {
+        try
+        {
+            // путь к существующему файлу
+            const string path = "Public/vk.jpg";
+            // создаём поток данных, чтобы не забивать память единоразово, если файл большой
+            var fileStream = new FileStream(path, FileMode.Open);
+            // устанавливаем тип контента
+            const string contentType = "image/jpeg";
+            // задаём название скачиваемому файлу (может отличаться от "базового" названия)
+            const string downloadName = "vk.jpg";
+            // возвращаем файл
+            return Results.File(fileStream, contentType, downloadName);
+        }
+        catch (Exception e)
+        {
+            // в случае ошибки возвращаем статус-код 400 с сообщением "Ошибка"
+            Console.WriteLine(e);
+            return Results.BadRequest(e);
+        }
+    }
+}
---
+++ Backend-lection-EF/Services/HtmlResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Backend_lection_EF.Services;
+
+public class HtmlResult(string htmlCode) : IResult
+{
+    public async Task ExecuteAsync(HttpContext context)
+    {
+        // устанавливаем заголовок, кодировку и отправляем html
+        context.Response.ContentType = "text/html; charset=utf-8";
+        await context.Response.WriteAsync(htmlCode);
+    }
+}
+
+static class ResultsExtensions
+{
+    // создаём метод расширения (для более гибкого управления возможными ответами), который будем использовать при отправке html
+    public static IResult Html(this IResultExtensions resultExtensions, string html)
+    {
+        // создаём исключение, если resultExtensions - null;
+        ArgumentNullException.ThrowIfNull(resultExtensions);
+        return new HtmlResult(html);
+    }
+}
